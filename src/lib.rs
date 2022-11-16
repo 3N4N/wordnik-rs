c3c9ec54eb39e1f2dd9fa8cb1471a6e5d06aebdf
Enan Ajmain <3nan.ajmain@gmail.com>
2022-11-17 00:13:26 +0600

    feat(definitions): add fn returning defs as string

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,7 @@
+use serde::Deserialize;
+use serde::Serialize;
 use serde_json::Value;
+use std::collections::HashMap;
 use std::fmt::Display;
 
 /// The struct representing the API endpoint.
@@ -52,9 +55,6 @@ impl Display for Operation {
   }
 }
 
-use serde::Deserialize;
-use serde::Serialize;
-
 /// The struct representing API response for word definition.
 #[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
 #[serde(rename_all = "camelCase")]
@@ -68,6 +68,16 @@ pub struct Definition {
   pub source_dictionary: String,
   pub attribution_url: String,
   pub wordnik_url: String,
+}
+
+impl Definition {
+  fn to_pretty(&self) -> String {
+    if self.part_of_speech.clone() == "" {
+      self.definition.clone() + "\n"
+    } else {
+      self.part_of_speech.clone() + " " + &self.definition + "\n"
+    }
+  }
 }
 
 impl Wordnik {
@@ -91,6 +101,9 @@ impl Wordnik {
   }
 
   /// Get definitions of a word.
+  ///
+  /// For a pretty string of definitions,
+  /// see [get_definitions_pretty](Self::get_definitions_pretty).
   ///
   /// # Example
   ///
@@ -118,5 +131,54 @@ impl Wordnik {
     let res = self.make_request(url)?;
     let definitions: Vec<Definition> = serde_json::from_value(res)?;
     Ok(definitions)
+  }
+
+  /// Get definitions of a word.
+  ///
+  /// # Example
+  ///
+  /// ```
+  /// use wordnik::Wordnik;
+  ///
+  /// let api = Wordnik::new(
+  ///   "YOUR_API_KEY".to_string(),
+  ///   "https://api.wordnik.com/v4/word.json/".to_string(),
+  /// );
+  ///
+  /// let v = api.get_definitions_pretty("word").unwrap();
+  /// println!("{}", v);
+  /// ```
+  pub fn get_definitions_pretty(
+    &self,
+    word: &str,
+  ) -> Result<String, Box<dyn std::error::Error>> {
+    let definitions = self.get_definitions(word)?;
+    let mut hm_definitions: HashMap<String, Vec<Definition>> = HashMap::new();
+    for i in &definitions {
+      if hm_definitions.contains_key(&i.attribution_text) {
+        let v = hm_definitions.get_mut(&i.attribution_text).unwrap();
+        v.push(i.clone());
+      } else {
+        let mut v: Vec<Definition> = Vec::new();
+        v.push(i.clone());
+        hm_definitions.insert(i.attribution_text.clone(), v);
+      }
+    }
+
+    let mut s = "".to_string();
+    for (attribution_text, definitions) in hm_definitions.iter() {
+      s = s
+        + attribution_text
+        + "\n"
+        + definitions
+          .iter()
+          .fold("".into(), |acc, d| {
+            [acc, "  * ".into(), d.to_pretty()].join("")
+          })
+          .as_str()
+        + "\n";
+    }
+
+    Ok(s)
   }
 }
